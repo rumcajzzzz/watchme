e29379e6dc7699b9814ea4d2552789bad13318a4
Marcel Niemiec <marcel.niemiec6@gmail.com>
2025-11-13 22:10:24 +0100

    fix: mobile block zoom/pinch

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,12 @@ interface RootLayoutProps {
 export default function RootLayout({ children, nickname }: RootLayoutProps) {
   return (
     <html lang="en" className="bg-black text-white">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+      </head>
       <body className={`${geist.className} font-sans antialiased relative`}>
         <Header nickname={nickname} />
         <main className="min-h-screen w-full flex flex-col items-center justify-center">
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,22 +1,31 @@
+"use client"
+
 import Link from "next/link"
 import Image from "next/image"
+import { usePathname } from "next/navigation"
 
 export default function Footer() {
+  const pathname = usePathname()
+  if (pathname.startsWith("/view/")) return null
+
   return (
     <footer className="fixed bottom-4 left-0 w-full flex justify-center pointer-events-none">
       <Link
         href="https://rumcajzdev.pl"
         target="_blank"
-        className="flex items-center gap-2 pointer-events-auto px-4 py-2 rounded-xl bg-gray-700/10 backdrop-blur-md drop-shadow-[0_0_24px_rgba(0,0,0,0.9)]"
+        className="flex items-center gap-1 pointer-events-auto px-2 py-1 rounded-lg bg-gray-700/10 backdrop-blur-md drop-shadow-[0_0_24px_rgba(0,0,0,0.9)] sm:px-4 sm:py-2 sm:gap-2 sm:rounded-xl"
       >
         <Image
           src="/rumcajzdevlogowhite.png"
           alt="Logo"
-          width={32}
-          height={32}
-          className="opacity-95 drop-shadow-[0_0_16px_rgba(0,0,0,0.9)]"
+          width={18} 
+          height={18}
+          className="opacity-95 drop-shadow-[0_0_16px_rgba(0,0,0,0.9)] sm:w-6 sm:h-6"
         />
-        <span className="text-secondary text-s tracking-widest uppercase drop-shadow-[0_0_20px_rgba(0,0,0,0.9)]">
+        <span 
+          // Zmniejszony rozmiar tekstu na małych ekranach (np. 'xs' zamiast 's')
+          className="text-secondary text-xs tracking-widest uppercase drop-shadow-[0_0_20px_rgba(0,0,0,0.9)] sm:text-sm"
+        >
           developed by rumcajzdev
         </span>
       </Link>
